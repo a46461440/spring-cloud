607a9aa14f781f3f46bade73e1bb9af7de4178e9
ZXC <496461440@qq.com>
2018-09-07 18:01:20 +0800

    actuator

--- src/main/java/com/zxc/springcloud/action/HelloWorldAction.java
+++ src/main/java/com/zxc/springcloud/action/HelloWorldAction.java
@@ -1,18 +1,26 @@
 package com.zxc.springcloud.action;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldAction {
 
-    @Value("${book.name}")
-    private String bookName;
+    private Log log = LogFactory.getLog(this.getClass());
+
+    @Autowired
+    public DiscoveryClient client;
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello " + this.bookName;
+//        ServiceInstance instance = (ServiceInstance) this.client.getInstances("server-one");
+//        this.log.info(instance.getHost() + ":" + instance.getServiceId());
+        return "hello ";
     }
 
 }
